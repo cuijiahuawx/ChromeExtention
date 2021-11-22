4ed4735f67ee3f5a4c4c8f580aaa5c0cc165f140
cuijiahua <2509605080@qq.com>
2021-11-23 02:42:56 +0800

    可用

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(function() 
 {
+  // $("div.ga-click.u-navLogin-myCourse > div > div > a > span").text("我的课程");
+
+  // var something = '<div><button type="button" class="btn btn-primary click">Primary</button></div>';
+  // $("div.u-navLogin-container").append(something);
+  
+  // $(".click").click(function(){
+  //   console.log("yes");
+  // });
+  
   /*
   console.log("loaded");
 
